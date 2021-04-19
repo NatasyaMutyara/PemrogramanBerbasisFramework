8221d6d1b384a789b4bd04ae5b4f9378019a83dd
NatasyaMutyara <54783669+NatasyaMutyara@users.noreply.github.com>
2021-04-19 08:08:46 +0700

    modul 10

---
+++ pbf-modul10/src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default{
+    apiKey: "AIzaSyC6oE6fphMf71jRydYSy_bMrY3AndG9cl8",
+    authDomain: "pbf-modul10-a404f.firebaseapp.com",
+    projectId: "pbf-modul10-a404f",
+    storageBucket: "pbf-modul10-a404f.appspot.com",
+    messagingSenderId: "240586786549",
+    appId: "1:240586786549:web:e5f0cd243993ed5ca6e7bc",
+    measurementId: "G-5ZGHGF1T70"
+  };
